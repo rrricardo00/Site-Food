df6ec070e2da2d9130b5c6d1e62c8f3be0365a8f
rrricardo00 <rrricardo00@yahoo.com.br>
2021-05-01 02:24:26 -0300

    v1.0

--- _js/js.js
+++ _js/js.js
@@ -1,3 +1,29 @@
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     document.documentElement.scrollTop > 100 ? document.querySelector('.menu').style.background = "#000" : document.querySelector('.menu').style.backgroundColor = "rgba(0,0,0,.5)";
 })
+
+function mostrarScroll() {
+    
+    function show(e) {
+        
+        window.addEventListener('scroll', () =>{
+            
+            const telaTop = window.scrollY;
+            const elementoTop = e.getBoundingClientRect().top;
+            
+            telaTop < elementoTop ? e.style.opacity = "0" : e.style.opacity = "1";
+
+        });
+    }
+
+    function pegarItem(){
+        document.querySelectorAll('section').forEach(item => show(item))
+    }
+
+    pegarItem();
+
+}
+
+
+
+mostrarScroll();
